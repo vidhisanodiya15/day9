b9b6ee336168b289072297f47485e9faed22339c
Vidhi sanodiya <114800338+vidhisanodiya15@users.noreply.github.com>
2024-01-09 21:10:47 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+# x=5
+# y=7.6
+# z=4j
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x=4.5
+# y=34.5
+# z=-3.4
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+import random
+
+print (random.randrange(1,20))
